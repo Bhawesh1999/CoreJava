c75a6f85ad20ed6f03c3263d9422fb8898a0853a
Bhawesh Chandrakant Parate <84832768+Bhawesh1999@users.noreply.github.com>
2024-04-26 21:43:24 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+package CoreJava;
+
+public class Arrays 
+{
+	public static void main(String[] args) {
+		int[][] marks = {
+				{67,58,48,93,94},
+				{83,45,32,11,45},
+				{12,35,64,32,54}};
+	for(int[] row:marks)
+	{
+		for(int mark :row)
+	{
+		System.out.println(marks);
+	}
+}
+	}
+}
